01d55403fd981cdc875e7a97bbb2a3a0402b09d5
윤승재 <tmdwo5811@gmail.com>
2024-11-11 15:26:45 +0900

    [master] const warning 제거, 아이콘 리스트 로직 추가

--- au_keep_app/lib/main.dart
+++ au_keep_app/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:win32/win32.dart'; // https://pub.dev/packages/win32
 
 void main() { // 메인 함수
   runApp(const MyApp()); // 앱 시작, 메인 페이지
@@ -11,23 +10,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
 
-    // 이곳부터 코딩 시작, 디자인 등
+    // // 이곳부터 코딩 시작, 디자인 등
+    //
+    // // 위젯을 짜집기 한다.
+    //
+    // // 가장 중요한 4개 위젯
+    //
+    // // 글자 위젯
+    // Text text = Text('나는 지안이를 사랑해\n지금 갈게ㅎ 사랑해사랑해');
+    // // 이미지 위젯,
+    // Image image = Image.asset('img.png');
+    // // 아이콘 위젯
+    // Icon icon = Icon(Icons.star);
+    // // 박스 위젯 2종, 플러터의 사이즈 단위는 LP이다. px이 아님, 50LP == 1.2cm 정도
+    // Container container = Container(width: 500, height: 500, color: Colors.blue, child: text);
+    // SizedBox sizedBox = SizedBox();
 
-    // 위젯을 짜집기 한다.
 
-    // 가장 중요한 4개 위젯
-
-    // 글자 위젯
-    Text text = Text('나는 지안이를 사랑해\n지금 갈게ㅎ 사랑해사랑해');
-    // 이미지 위젯,
-    Image image = Image.asset('img.png');
-    // 아이콘 위젯
-    Icon icon = Icon(Icons.star);
-    // 박스 위젯 2종, 플러터의 사이즈 단위는 LP이다. px이 아님, 50LP == 1.2cm 정도
-    Container container = Container(width: 500, height: 500, color: Colors.blue, child: text);
-    SizedBox sizedBox = SizedBox();
+    // Center(child: container); // 위젯 내부에 위젯을 위치하고 싶을때 child 를 사용한다.
     return MaterialApp(
-      home: Center(child: container) // 위젯 내부에 위젯을 위치하고 싶을때 child 를 사용한다.
+      home: Scaffold(
+        body: Column(
+          children: [
+            Icon(Icons.star),
+            Icon(Icons.star),
+            Icon(Icons.star)
+          ],
+        ),
+        // bottomNavigationBar: BottomAppBar(),
+      )
     );
   }
 }
